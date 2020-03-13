dc168674986e1dcdbd35ef7995ef931371f8d774
Matt <gunhodarro@gmail.com>
2020-03-12 19:23:52 -0600

    changes so deploy works

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+var MONGODDB_URI = process.env.MONGODDB_URI || "mongod://localhost/mongoHeadlines"
 const PORT = 3000;
 
 const app = express();
@@ -14,13 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+mongoose.connect(MONGODDB_URI);
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes here
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
